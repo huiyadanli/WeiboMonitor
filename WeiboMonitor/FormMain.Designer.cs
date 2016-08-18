9585a9be561bf7b745697b1ad4fa5b6c3a886cd1
huiyadanli <huiyadanli@gmail.com>
2016-08-18 19:27:16 +0800

    rename notifyIcon1

--- WeiboMonitor/FormMain.Designer.cs
+++ WeiboMonitor/FormMain.Designer.cs
@@ -148,7 +148,7 @@
             // 
             this.notifyIcon1.ContextMenuStrip = this.contextMenuStrip1;
             this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
-            this.notifyIcon1.Text = "notifyIcon1";
+            this.notifyIcon1.Text = "新发微博秒赞器";
             this.notifyIcon1.Visible = true;
             this.notifyIcon1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon1_MouseDoubleClick);
             // 
